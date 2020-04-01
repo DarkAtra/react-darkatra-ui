c73178dc03f94de1aa6459e1f283d8dd6c9f4a58
Tommy Schmidt <darkatra@web.de>
2020-04-01 23:43:56 +0200

    added demo via github pages

---
+++ docs/precache-manifest.f61363f4162f2759f0d7ce6a5c388d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4192e17cde194f66c55ea2590db25ebf",
+    "url": "/react-darkatra-ui/index.html"
+  },
+  {
+    "revision": "009c3337bfb02a89578e",
+    "url": "/react-darkatra-ui/static/js/2.b3dccddb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-darkatra-ui/static/js/2.b3dccddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b56585a60a2bc608f24",
+    "url": "/react-darkatra-ui/static/js/main.345e38ac.chunk.js"
+  },
+  {
+    "revision": "5e03756c2ca2213e9f01",
+    "url": "/react-darkatra-ui/static/js/runtime-main.5aba99f9.js"
+  }
+]);
